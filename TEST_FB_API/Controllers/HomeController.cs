d2adfe6ea49a61c42a9c9ba1ba539cf82a71d30c
Vezral <Kenji1358@yahoo.com>
2019-04-04 11:56:28 +0800

    intermediary

--- TEST_FB_API/Controllers/HomeController.cs
+++ TEST_FB_API/Controllers/HomeController.cs
@@ -21,20 +21,6 @@ namespace TEST_FB_API.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [HttpPost]
         public ActionResult GetChartData()
         {
